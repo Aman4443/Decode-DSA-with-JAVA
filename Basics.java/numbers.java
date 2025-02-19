4c991d13025a0368cf527b7d06e322fb63a47c91
Aman jha <amankumarjha4444@gmail.com>
2025-02-19 22:38:33 +0530

    Printing Numbers

---
+++ Basics.java/numbers.java
@@ -0,0 +1,10 @@
+public class numbers {
+    public static void main(String[] args) {
+        //  Scannner sc = new Scanner(System.in);
+         // System.out.println("Enter the value");
+         // int n = sc.nextInt();
+          for (int i = 1; i <=100 ; i++) {
+              System.out.println(i);
+          }
+      }
+}
