bcd10599bd453467b7ec68b7e1e7d92f5b49c274
Aman jha <amankumarjha4444@gmail.com>
2025-02-23 23:24:52 +0530

    Array Basics

---
+++ Basics.java/Array/Arrbasics.java
@@ -0,0 +1,16 @@
+package Array;
+
+public class Arrbasics {
+    public static void main(String[]args){
+        int[]arr = new int[5]; // its is decelaration and memory allocation.
+        arr[0] = 1; // its is individually initialising array element.
+        arr[1] = 12;
+        arr[2] = 15;
+        arr[3] = 14;
+        arr[4] = 11;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+    }
+}
