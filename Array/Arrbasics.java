83030c4a90a223e0293605dd3c91b4537c32affe
Aman jha <amankumarjha4444@gmail.com>
2025-02-28 22:38:39 +0530

    Array basics

--- Array/Arrbasics.java
+++ Array/Arrbasics.java
@@ -11,6 +11,6 @@ public class Arrbasics {
         System.out.println(arr[0]);
         System.out.println(arr[1]);
         System.out.println(arr[2]);
-        System.out.println(arr[3]);
+        
     }
 }
