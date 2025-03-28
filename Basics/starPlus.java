8d13cc322d175028395da87b22c7cf0b3ea8e6b6
AMAN KUMAR JHA <95953071+Aman4443@users.noreply.github.com>
2025-03-28 23:55:03 +0530

    Update starPlus.java

--- Basics/starPlus.java
+++ Basics/starPlus.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// WAP to Print the Design in star Pattern
 public class starPlus {
      public static void main(String[]args) {
         Scanner sc = new Scanner(System.in);
